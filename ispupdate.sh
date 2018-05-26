5901adae08802cb4feec718355ebd885ef34b0fc
bostonaqua <kiyaschenko@gmal.com>
2018-05-26 09:05:52 +0300

    five

--- ispupdate.sh
+++ ispupdate.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 conf=$(echo "/usr/local/ispmgr/etc/ispmgr.conf")
 bit=$(uname -m | sed s/_/-/g)
-php=$(php -v | awk 'NR==1{print substr($2,1,3)}')
 echo -e "\nIn progress..."
 sed -i 's/enabled=1/enabled=0/g' /etc/yum.repos.d/atrpms.repo
 if ! grep -q "Option DisableIspmgrEOLBannersDisplay" $conf
@@ -10,6 +9,7 @@ if ! grep -q "FSEncoding UTF-8" $conf
 then echo "FSEncoding UTF-8" >> $conf; fi
 yum update -y -q 1>/dev/null
 yum install php-gd php-mcrypt php-curl php-soap php-bcmath -y -q 1>/dev/null
+php=$(php -v | awk 'NR==1{print substr($2,1,3)}')
 wget -q "http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_$bit.tar.gz"
 if ! [ -d /usr/local/ioncube/ ]; then mkdir /usr/local/ioncube/; fi
 tar zxf "ioncube_loaders_lin_$bit.tar.gz" -C /usr/local/ioncube/ --strip 1
